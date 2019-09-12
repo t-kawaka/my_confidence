b71f9ffe10dab0ab2d3d88c7e046feccf7abe9e1
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-13 02:15:24 +0900

    add:point各画面作成（途中）

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,10 +1,46 @@
 class PointsController < ApplicationController
+  before_action :set_point, only: %i[edit update destroy]
+
   def new
+    @point = Point.new
   end
 
-  def show
+  def create
+    @task = Task.find(params[:task_id])
+    @point = @task.points.build(point_params)
+    if @point.save
+      flash[:notice] = "重点的に取り組んでいること「#{@point.name}」を作成しました"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @point.update(task_params)
+      flash[:notice] = "重点的に取り組んでいること「#{@point.name}」を編集しました"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @point.destroy
+    flash[:notice] = "重点的に取り組んでいること「#{@point.name}」を削除しました"
+    redirect_to tasks_path
+  end
+
+  private
+
+  def point_params
+    params.require(:comment).permit(:task_id, :name)
+  end
+
+  def set_point
+    @point = Point.find(params[:id])
   end
 end
