0d829841ca43ddf58cadad7455dcc81d4e6c6bc3
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-16 21:02:16 +0900

    add:commentテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_005402) do
+ActiveRecord::Schema.define(version: 2019_09_16_115733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.bigint "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "points", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +61,8 @@ ActiveRecord::Schema.define(version: 2019_09_16_005402) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tasks"
+  add_foreign_key "comments", "users"
   add_foreign_key "points", "tasks"
   add_foreign_key "tasks", "users"
 end
