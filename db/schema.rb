02a4b7d1df17c026e7f427443f557cfc92563d4a
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-18 02:49:38 +0900

    add:tagのモデルとactionとtagの中間テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_115733) do
+ActiveRecord::Schema.define(version: 2019_09_17_174748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2019_09_16_115733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_points_on_task_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", default: "プログラミング", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "task_tags", force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
