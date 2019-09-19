00e9e376475294a65c79a7a5043967b23f8e5cd9
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-19 16:51:31 +0900

    add:rspec関係のgem導入

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,15 @@ module MyConfidence
     config.active_record.default_timezone = :local
     I18n.config.available_locales = :ja
     I18n.default_locale = :ja
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
