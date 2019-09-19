dcf580c3873666a4cee43d9630903440335e5c98
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-19 12:09:06 +0900

    modify:controller いいね（task_favorite）修正

--- app/controllers/task_favorites_controller.rb
+++ app/controllers/task_favorites_controller.rb
@@ -1,7 +1,9 @@
 class TaskFavoritesController < ApplicationController
 
   def create
-    task_favorite = current_user.task_favorites.create(task_id: params[:task_id])
+    @task = Task.find(params[:task_id])
+    task_favorite = current_user.task_favorites.build(task_id: params[:task_id])
+    task_favorite.save
     redirect_to tasks_path, notice: "#{task_favorite.task.user.name}さんの投稿をお気に入り登録しました"
   end
 
