4da340bb1f5b9ac2c9227b2218418d16ddd1dd0d
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-20 11:00:41 +0900

    add:rspec pointsの情報更新（登録失敗例追加）

--- spec/features/point_spec.rb
+++ spec/features/point_spec.rb
@@ -17,5 +17,17 @@ require 'rails_helper'
         click_button '保存'
         expect(page).to have_content '重点的に取り組んでいること「成果物作成する」を作成しました'
       end
+
+      it "「重点的に取り組んでいること」のタイトルを入力していない場合" do
+        visit new_user_session_path
+        fill_in "メールアドレス", with: "test1@gmail.com"
+        fill_in "パスワード", with: "password"
+        click_button 'ログイン'
+
+        visit new_point_path
+        fill_in "最近重点的に取り組んでいる内容", with: nil
+        click_button '保存'
+        expect(page).to have_content '1件のエラーがあります。'
+      end
     end
   end
