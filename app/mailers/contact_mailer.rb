f802c8e3dbde79b470379fce392e6743c358d17e
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-28 22:14:24 +0900

    add:本番環境でのメール送信機能の作成

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ApplicationMailer
   default from: 'my_confidence@example.com'
-  
+
   def contact_mail(contact)
     @contact = contact
 
-    mail(to: @contact.user.email, subject: 'お問い合わせありがとうございます')
+    mail(to: "chorin.channel1136@gmail.com", subject: 'お問い合わせありがとうございます')
   end
 end
