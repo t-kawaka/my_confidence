4802f2fa434edebe72e07b6a849762fd85484896
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-09-30 01:57:40 +0900

    modify:いいね機能のAjax化（途中）

--- app/controllers/task_favorites_controller.rb
+++ app/controllers/task_favorites_controller.rb
@@ -1,12 +1,11 @@
 class TaskFavoritesController < ApplicationController
 
   def create
-    task_favorite = current_user.task_favorites.create(task_id: params[:task_id])
-    redirect_to tasks_path, notice: "#{task_favorite.task.user.name}さんの投稿をお気に入り登録しました"
+    @task = Task.find(params[:task_id])
+    @task_favorite = current_user.task_favorites.create(task_id: params[:task_id])
   end
 
   def destroy
-    task_favorite = current_user.task_favorites.find_by(id: params[:id]).destroy
-    redirect_to tasks_path, notice: "#{task_favorite.task.user.name}さんの投稿のお気に入りを解除しました"
+    @task_favorite = current_user.task_favorites.find_by(id: params[:id]).destroy
   end
 end
