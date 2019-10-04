e8941db7315d020ae025f5c5cfb005cf2b49a84a
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-10-05 03:58:33 +0900

    add:フォロー・つながりを解除作成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,16 +5,11 @@ class RelationshipsController < ApplicationController
       @user = User.find(params[:relationship][:followed_id])
       current_user.follow!(@user)
       @user # @userをjs.erbに渡すための記述です。
-      redirect_to users_path
-    else
-      @user
-    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
     @user
-    redirect_to users_path
   end
 end
