d69fbc7c30bc8bc569c4829a372426b573cc1d30
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-10-06 22:21:41 +0900

    modify:登録ユーザー一覧修正（途中）

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -18,7 +18,7 @@ FactoryBot.define do
   factory :task do
     title { 'MyConfidence' }
     description { '文章作成' }
-    notice {nil}
+    notice { nil }
     start_time { DateTime.now }
     require_time { 10 }
     progress { '開始' }
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -15,7 +15,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   before do
     @user = FactoryBot.create(:user)
-    @task = Task.create(title: 'アクション作成', description: 'アクション詳細', start_time: Date.current , require_time:10, progress: '開始', user_id: @user.id)
+    @task = Task.create(title: 'アクション作成', description: 'アクション詳細', start_time: Date.current, require_time: 10, progress: '開始', user_id: @user.id)
   end
 
   it "現在重点的に取り組んでいること" do
@@ -28,7 +28,7 @@ RSpec.describe Comment, type: :model do
   it "commentのcontentが未入力であった場合" do
     user = @user
     task = @task
-    comment = Comment.new(content:nil, user_id: @user.id, task_id: @task.id)
+    comment = Comment.new(content: nil, user_id: @user.id, task_id: @task.id)
     expect(comment).to be_invalid
   end
 end
