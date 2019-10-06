ca34cabe92a415cd197a58f698443aa930b78097
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-10-06 21:02:26 +0900

    add:Rspecのmodel（contact・relationship・task_favorites）作成

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  it 'お問い合わせ作成' do
+    user = @user
+    contact = Contact.new(title: 'お問い合わせタイトル', content: 'お問い合わせ内容', user_id: @user.id)
+    expect(contact).to be_valid
+  end
+
+  it 'titleが未入力であった場合' do
+    user = @user
+    contact = Contact.new(title: nil, content: 'お問い合わせ内容', user_id: @user.id)
+    expect(contact).to be_invalid
+  end
+
+  it 'contentが未入力であった場合' do
+    user = @user
+    contact = Contact.new(title: 'お問い合わせタイトル', content: nil, user_id: @user.id)
+    expect(contact).to be_invalid
+  end
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @user2 = User.create(name: 'user2', email: 'test2@gmail.com', password: 'password')
+  end
+
+  it 'ユーザーへの「いいね！」設定' do
+    relationship = Relationship.new(
+      follower_id: @user.id,
+      followed_id: @user2.id
+    )
+    relationship.valid?
+    expect(relationship).to be_valid
+  end
 end
