bd451dad8a9d53c2add53ea4cf81a42a7af109b1
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-10-12 00:16:25 +0900

    modify:プロフィール一部修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,2 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, only: [:create]
-
-  protected
-
-  def configure_permitted_parameters
-    added_attrs = [:name, :admin, :icon, :remove_icon]
-    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-  end
 end
