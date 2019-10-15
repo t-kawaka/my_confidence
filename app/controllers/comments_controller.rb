6270294e2506a78548dd6e48efdb539202f52944
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-10-15 21:01:40 +0900

    modify:コメントの編集画面作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_task, only: %i[destroy]
-  before_action :set_comment, only: %i[destroy]
+  before_action :set_task, only: %i[edit update destroy]
+  before_action :set_comment, only: %i[edit update destroy]
 
   def create
     @task = Task.find(params[:task_id])
@@ -16,8 +15,24 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    if @comment.user_id != current_user.id
+      redirect_to tasks_path, notice: "コメントの編集許可がありません"
+    end
+  end
+
+  def update
+    if @comment.user_id == current_user.id
+      if @comment.update(comment_params)
+        redirect_to task_path(@task), notice: "コメントが更新されました"
+      else
+        flash[:alert] = "コメントが空欄になっています。"
+        render :edit
+      end
+    end
+  end
+
   def destroy
-    @comment = Comment.find(params[:id])
     if @comment.destroy
       respond_to do |format|
         format.js { render :index }
@@ -28,11 +43,11 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:task_id, :content, :user_id)
+    params.require(:comment).permit(:task_id, :content)
   end
 
   def set_task
-    @task = Task.find(params[:id])
+    @task = Task.find(params[:task_id])
   end
 
   def set_comment
