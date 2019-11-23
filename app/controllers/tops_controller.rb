62655d6271ceb21ac7b685b1a8d549d4e2fde23e
川上　貴弘 <chorin@kawakamitakahironoMacBook-Pro.local>
2019-11-23 14:38:22 +0900

    add:ログイン前のページに過去のアクション欄を記入

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,9 @@
 class TopsController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to tasks_path
+    else
+      @tasks = Task.all.order(created_at: :desc).limit(3)
+    end
   end
 end
